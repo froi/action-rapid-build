a4c7199f9a78c010003bdd62b7642e3cb19a0924
Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>
2021-02-25 00:35:18 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.info(github.context.payload)
   } catch (error) {
     core.setFailed(error.message);
   }
