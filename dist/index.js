8933b3ed0e23d17e91f96f03a28dd50199602092
Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>
2021-02-25 01:00:49 -0500

    Add more stuff

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,19 @@ const github = __nccwpck_require__(438);
 async function run() {
   try {
     core.info(JSON.stringify(github.context.payload, null, 2));
+    const sender = github.context.payload.sender.login;
+    const owner = github.context.repo.owner.login;
+    const repo = github.context.repo.name;
+    const issueNumber = github.context.issue.number;
+
+    const octokit = github.getOctokit();
+
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: `Hey @${sender}. Why you label me?`
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const github = require('@actions/github');
 async function run() {
   try {
     core.info(JSON.stringify(github.context.payload, null, 2));
+    const sender = github.context.payload.sender.login;
+    const owner = github.context.repo.owner.login;
+    const repo = github.context.repo.name;
+    const issueNumber = github.context.issue.number;
+
+    const octokit = github.getOctokit();
+
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: `Hey @${sender}. Why you label me?`
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
